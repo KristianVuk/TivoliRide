0d0a7a70b12f91a6d308fa804b0f360ea2b12f3b
lindkvst <lindkviste@gmail.com>
2025-09-03 10:47:25 +0200

    Test2

--- src/Tivoli01/TivoliRide.java
+++ src/Tivoli01/TivoliRide.java
@@ -1,4 +1,17 @@
 package Tivoli01;
 
 public class TivoliRide {
+    //jeg laver en Ride klasse
+    private String rideName;
+    private int userHeight;
+
 }
+
+//jeg laver konstruktør til at oprette Rides
+
+public TivoliRide(String rideName, int userHeight) {
+    this.rideName = rideName;
+    this.userHeight = userHeight;
+}
+
+//
