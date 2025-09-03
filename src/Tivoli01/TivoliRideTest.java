15135bc0f38f2898c32c7b0f3a30ce705ec04428
KristianVuk <krvu1000@stud.ek.dk>
2025-09-03 11:58:43 +0200

    Test færdig

--- src/Tivoli01/TivoliRideTest.java
+++ src/Tivoli01/TivoliRideTest.java
@@ -1,5 +1,20 @@
 package Tivoli01;
 
-public class TivoliRideTest { //Hej .
-
+public class TivoliRideTest {
+    public static void main(String[] args) {
+        CreateRide();
+    }
+    public static void CreateRide() {
+       //Opret Ride
+        TivoliRideMain r1 = new TivoliRideMain("Demon", 150);
+        TestHeight(r1);
+    }
+    public static void TestHeight(TivoliRideMain r) {
+        int height = 160;
+        if (height >= r.getRideHeight()){
+            System.out.println("Du er høj nok");
+        } else {
+            System.out.println("Gå hjem igen");
+        }
+    }
 }
