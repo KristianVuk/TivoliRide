b8bbe1d8890af4e76e97afeb4c0fd6c96f602dca
lindkvst <lindkviste@gmail.com>
2025-09-03 10:58:23 +0200

    Oprettet konstruktør

--- src/Tivoli01/TivoliRideMain.java
+++ src/Tivoli01/TivoliRideMain.java
@@ -4,6 +4,16 @@ public class TivoliRideMain {
     //jeg opretter klasse for rides
     private String rideName;
     private int rideHeight;
+
+    //jeg laver en konstruktør til input af data
+
+    public TivoliRideMain {
+        this.rideName = rideName;
+        this.rideHeight = rideHeight;
+    }
+
 }
+
+
 
 //dette er en test.
